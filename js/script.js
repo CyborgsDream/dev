d96a62cc31cabf43c22054743a03ab678cdfe83f
CyborgsDream <cyborgsdream333@gmail.com>
2025-07-27 17:15:49 +0200

    Bump version to 0.0.14g

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Version: 0.0.14f
+// Version: 0.0.14g
 // Codename: Celestia
 // Basic THREE.js example with multiple objects
 import * as THREE from 'https://unpkg.com/three@0.159.0/build/three.module.js';
